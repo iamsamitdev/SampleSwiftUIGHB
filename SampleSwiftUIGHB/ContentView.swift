c1093a870b47594389e6ef06ae25f9bf098a8774
Samit Koyom <samitkoyom@gmail.com>
2025-05-26 21:42:28 +0700

    initial commit

--- SampleSwiftUIGHB/ContentView.swift
+++ SampleSwiftUIGHB/ContentView.swift
@@ -13,7 +13,11 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to SwiftUI!")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+                .padding()
         }
         .padding()
     }
