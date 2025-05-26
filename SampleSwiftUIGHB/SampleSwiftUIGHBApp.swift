1b191d7b437b3edb0c1104b25c60a951c422688a
Samit Koyom <samitkoyom@gmail.com>
2025-05-26 21:03:19 +0700

    Initial Commit

---
+++ SampleSwiftUIGHB/SampleSwiftUIGHBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIGHBApp.swift
+//  SampleSwiftUIGHB
+//
+//  Created by Samit Koyom on 26/5/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIGHBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
