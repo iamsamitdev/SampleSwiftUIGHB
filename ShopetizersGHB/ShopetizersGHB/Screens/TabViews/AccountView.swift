69b35c28c37773f786800315f6b5d0d9c9be2ac4
Samit Koyom <samitkoyom@gmail.com>
2025-06-17 15:38:14 +0700

    add shopetizerGHB14

--- ShopetizersGHB/ShopetizersGHB/Screens/TabViews/AccountView.swift
+++ ShopetizersGHB/ShopetizersGHB/Screens/TabViews/AccountView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @StateObject var viewModel = AccountViewModel()
+    
     var body: some View {
-        Text("Account View")
+        NavigationView {
+            Form {
+                Section(
+                    header: Text("Personal Info")
+                        .padding(.leading, -20)
+                        .padding(.bottom, 5)
+                ){
+                    TextField("First Name", text: $viewModel.user.firstname)
+                    TextField("Last Name", text: $viewModel.user.lastname)
+                    TextField("Email", text: $viewModel.user.email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birth Date",
+                               selection: $viewModel.user.birthdate,
+                               displayedComponents: .date)
+                    
+                    Button {
+                        // print("Saved")
+                        viewModel.saveChanges()
+                    } label: {
+                        Text("Save Change")
+                    }
+                }
+                
+                Section(
+                    header: Text("Requests")
+                        .padding(.leading, -20)
+                        .padding(.bottom, 5)
+                ){
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
+                }
+                .navigationTitle("🤣 Account")
+            }
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
+                      dismissButton: alertItem.dismissButton)
+            }
+            .onAppear {
+                viewModel.retrieveUser()
+            }
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
-#Preview {
-    AccountView()
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
 }
